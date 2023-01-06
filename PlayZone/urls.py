836eed6a85083a1dd0563b93ba1cb1e006e0b74d
colmak <69098858+colmak@users.noreply.github.com>
2023-01-05 22:38:45 -0500

    Create connectFour app

--- PlayZone/urls.py
+++ PlayZone/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+from connectFour_App import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('playzone_App.urls')),
     path('account/', include('account_App.urls')),
+    path('', views.connectFour, name="connectFour"),
 ]
